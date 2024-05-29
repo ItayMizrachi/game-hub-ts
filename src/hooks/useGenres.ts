a452be67ef24888804b6772dd289d0a562c02377
ItayMizrachi <itaymiz98@gmail.com>
2024-05-30 02:10:35 +0300

    customized theme to get darker gray colors

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,11 +1,14 @@
-import useData from "./useData";
-
+import genres from "../data/genres";
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const useGenres = () => useData<Genre>("/genres");
+//using stored data because it barely changes to optimize the client expirience
+const useGenres = () => ({ data: genres, isLoading: false, error: null });
+
+// calling server for data
+// const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;
